6d3900d55f83dd6cc2c5406569976a4c55f9e151
ezrafrary <91634826+ezrafrary@users.noreply.github.com>
2024-10-09 01:00:42 -0700

    step 3

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import "./style.css";
 
+
+const framerate = 30;
+
 const app: HTMLDivElement = document.querySelector("#app")!;
 
 const gameName = "Ezra's amazing game";
@@ -9,6 +12,10 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+
+
+
+//button1
 const button1 = document.createElement("button");
 button1.innerHTML = "click";
 button1.style.position = "absolute"; //this came from brace
@@ -20,12 +27,41 @@ const displayScore = document.createElement("h2");
 displayScore.innerHTML = "0";
 app.append(displayScore);
 
-
 let num_clicks = 0;
 button1.onclick = () => {
     num_clicks++;
-    displayScore.innerHTML = num_clicks.toString();
 };
 
 
+let incrementalDivisor = 0;
+//button2
+const button2 = document.createElement("button");
+button2.innerHTML = "auto-button";
+button2.style.position = "absolute";
+button2.style.top = "550px";
+app.append(button2);
+
+button2.onclick = () =>{
+    incrementalDivisor++;
+    button2.innerHTML = `auto-button: (${incrementalDivisor})`
+};
+
+
+let increaseCooldown = framerate;
+
+
+
+setInterval(go, framerate);
+function go(){
+    if(incrementalDivisor != 0){
+        increaseCooldown = increaseCooldown - incrementalDivisor;
+        if(increaseCooldown < 0){
+            num_clicks++;
+            increaseCooldown = framerate;
+        }
+        
+    }
+    
+    displayScore.innerHTML = num_clicks.toString();
+};
 
