661992c4bb48c4a16f960ca332c56afe6da4c275
ezrafrary <91634826+ezrafrary@users.noreply.github.com>
2024-10-09 00:05:44 -0700

    step 1

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,12 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+
+
+
+const button1 = document.createElement("button");
+button1.innerHTML = "click";
+button1.style.position = "absolute"; //this came from brace 
+button1.style.top = "500px";
+app.append(button1);
